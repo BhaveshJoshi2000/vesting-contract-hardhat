2becb6b94736a077e4e7a90bd5626182dde5e48a
Bhavesh Joshi <89740838+BhaveshJoshi2000@users.noreply.github.com>
2022-11-02 21:54:24 +0530

    added code to update frontend variables

---
+++ deploy/02-update-frontend.js
@@ -0,0 +1,44 @@
+const { ethers, network } = require("hardhat");
+const fs = require("fs");
+
+const FRONT_END_ADDRESSES_FILE =
+  "../vesting-project-frontend/constants/contractAddresses.json";
+const FRONT_END_ABI_FILE = "../vesting-project-frontend/constants/abi.json";
+module.exports = async function () {
+  if (process.env.UPDATE_FRONT_END) {
+    console.log("updating frontend...");
+    await updateAbi();
+    await updateContractAddresses();
+  }
+};
+
+async function updateAbi() {
+  const vesting = await ethers.getContract("Vesting");
+  fs.writeFileSync(
+    FRONT_END_ABI_FILE,
+    vesting.interface.format(ethers.utils.FormatTypes.json)
+  );
+}
+async function updateContractAddresses() {
+  const vesting = await ethers.getContract("Vesting");
+  console.log(vesting);
+  const chainId = network.config.chainId.toString();
+  console.log(chainId);
+  const currentAddresses = JSON.parse(
+    fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8")
+  );
+  console.log(currentAddresses);
+  if (chainId in currentAddresses) {
+    if (!currentAddresses[chainId].includes(vesting.address)) {
+      console.log("updating contract address");
+      currentAddresses[chainId].push(vesting.address);
+    }
+  } else {
+    console.log("adding new address...");
+    currentAddresses[chainId] = [vesting.address];
+  }
+
+  fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses));
+}
+
+module.exports.tags = ["all", "frontend"];
